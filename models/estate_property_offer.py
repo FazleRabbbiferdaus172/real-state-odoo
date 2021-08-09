848e9731154cb6e7de0a084fd54d0c3ae2d45b61
ferdaus-bs23 <Rabbi.Ferdaus@brainstation-23.com>
2021-08-09 19:12:10 +0600

    chpt. 12 exercise 11

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -12,6 +12,7 @@ class PropertyOffer(models.Model):
     property_id = fields.Many2one(comodel_name="estate.property", required=True)
     validity = fields.Integer(string="Validity(days)", default=7)
     date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+    property_type_id = fields.Many2one(related='property_id.property_type_id', store=True)
 
     _sql_constraints = [
         ('check_eprice', 'CHECK(price > 0)',
--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields,models
+from odoo import fields,models,api
 
 class PropertyType(models.Model):
     _name = "estate.property.type"
@@ -6,8 +6,16 @@ class PropertyType(models.Model):
 
     name = fields.Char('Type',required=True)
     sequence = fields.Integer('Sequence', default=1, help="Used to order property type house.Higher the better")
-
     property_ids = fields.One2many(comodel_name="estate.property", inverse_name="property_type_id")
-
+    offer_ids = fields.One2many(comodel_name="estate.property.offer", inverse_name="property_type_id")
+    offer_count = fields.Integer(string="Offer Count",compute="_compute_offer_count")
     _sql_constraints = [('type_unique', 'unique(name)', 'Type already exists.')]
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids.mapped('id'))
+
+
+
     
